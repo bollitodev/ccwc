e90e8ce5d76250a1459677db5338d3ba9676da45
Daniel Ramos <daniel.ramos.rivera.dev@gmail.com>
2026-05-07 16:17:16 -0700

    feat: update counting bytes option and add counting lines option

--- src/ccwc/ccwc.py
+++ src/ccwc/ccwc.py
@@ -1,17 +1,34 @@
 import typer
 from typing_extensions import Annotated
 
-def ccwc(file_path:str, show_bytes: Annotated[bool, typer.Option("--bytest", "-c",help="Count how many bytes the file or stream of file contains")]= False):
-    bytes_count = 0
-    with open(file_path, "r") as f:
-        content = f.read()
 
-        if show_bytes:
-            bytes_count = len(content.encode('utf-8'))
+def ccwc(
+    file_path: str,
+    show_bytes: Annotated[
+        bool,
+        typer.Option(
+            "--bytes",
+            "-c",
+            help="Count how many bytes the file or stream of file contains",
+        ),
+    ] = False,
+    show_lines: Annotated[
+        bool, typer.Option("--lines", "-l", help="Count how many lines the text in the file contains")
+    ] = False,
+):
+    bytes_count = 0
+    lines_count = 0
+    with open(file_path, "rb") as f:
+        for line in f:
+            if show_bytes:
+                bytes_count += len(line)
+            if show_lines:
+                lines_count += 1
 
     if show_bytes:
         print(f"{bytes_count} {file_path}")
-        return bytes_count 
-    return (bytes_count)
-
-
+        return bytes_count
+    if show_lines:
+        print(f"{lines_count} {file_path}")
+        return lines_count
+    return bytes_count
