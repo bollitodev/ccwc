0511cfcd5327f2d0421e8b650acda2761a2f89c5
Daniel Ramos <daniel.ramos.rivera.dev@gmail.com>
2026-05-08 13:35:40 -0700

    feat: add word and car counter. ALso support system inputs

--- src/ccwc/ccwc.py
+++ src/ccwc/ccwc.py
@@ -1,10 +1,11 @@
 import typer
+import sys
 from typing_extensions import Annotated
 
 
 def ccwc(
-    file_path: str,
-    show_bytes: Annotated[
+        file_path: Annotated[str|None, typer.Argument()] = None,
+    bytes_c: Annotated[
         bool,
         typer.Option(
             "--bytes",
@@ -12,23 +13,64 @@ def ccwc(
             help="Count how many bytes the file or stream of file contains",
         ),
     ] = False,
-    show_lines: Annotated[
-        bool, typer.Option("--lines", "-l", help="Count how many lines the text in the file contains")
+    lines_c: Annotated[
+        bool,
+        typer.Option(
+            "--lines", "-l", help="Count how many lines the text in the file contains"
+        ),
     ] = False,
+    word_c: Annotated[
+        bool,
+        typer.Option("--words", "-w", help="Count how many words contains the file"),
+    ] = False,
+    chars_c: Annotated[
+        bool,
+        typer.Option("--chars", "-m", help="Count the amount of characters in the file")
+        ] = False
 ):
+    response = ""
     bytes_count = 0
     lines_count = 0
-    with open(file_path, "rb") as f:
-        for line in f:
-            if show_bytes:
-                bytes_count += len(line)
-            if show_lines:
-                lines_count += 1
+    words_count = 0
+    chars_count = 0
 
-    if show_bytes:
-        print(f"{bytes_count} {file_path}")
-        return bytes_count
-    if show_lines:
-        print(f"{lines_count} {file_path}")
-        return lines_count
-    return bytes_count
+    if not any([bytes_c, lines_c, word_c, chars_c]):
+        bytes_c = lines_c = word_c = True
+
+    if not file_path:
+        for line in sys.stdin:
+            if chars_c:
+                decoded_str = line.decode("utf-8")
+                chars_count += len(decoded_str)
+            if word_c:
+                decoded_str = line.decode("utf-8")
+                words_count += len(decoded_str.split())
+            if bytes_c:
+                bytes_count += len(line)
+            if lines_c:
+                lines_count += 1
+    else:
+        with open(file_path, "rb") as f:
+            for line in f:
+                if chars_c:
+                    decoded_str = line.decode("utf-8")
+                    chars_count += len(decoded_str)
+                if word_c:
+                    decoded_str = line.decode("utf-8")
+                    words_count += len(decoded_str.split())
+                if bytes_c:
+                    bytes_count += len(line)
+                if lines_c:
+                    lines_count += 1
+
+
+    if lines_c:
+        response = f" {lines_count}"
+    if word_c:
+        response = f"{response} {words_count}"
+    if bytes_c:
+        response = f"{response} {bytes_count}"
+    if chars_c:
+        response = f"{response} {chars_count}"
+
+    print(f"{response} {file_path if file_path else ''}")
